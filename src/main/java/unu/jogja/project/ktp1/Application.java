a5fd6582ea4888dfa59826ea08fad13842607f46
RivkyRiyantoro <sirivkyriyantoro@gmail.com>
2022-06-09 23:41:19 +0700

    percobaan 2

--- src/main/java/unu/jogja/project/ktp1/Application.java
+++ src/main/java/unu/jogja/project/ktp1/Application.java
@@ -2,9 +2,10 @@ package unu.jogja.project.ktp1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
